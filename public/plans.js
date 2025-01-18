5939af224d2399fabca8992b2cf138b516e170cc
polina <polvasilieva@list.ru>
2025-01-18 15:56:19 +0500

    ss

--- public/plans.js
+++ public/plans.js
@@ -4,8 +4,14 @@ $(document).ready(function () {
     Plans.forEach((item, index) => {
         $('#Plans').append(
             `<li class="second-item">
-                <img src="img/${item.img}" alt="${item.name}"> ${item.name}
-                <button class="removeFromPlans" data-index="${index}">Удалить</button>
+            <div class="block-cover">
+                <div class="book__card">
+                    <div class="book__card__info">
+                        <img src="img/${item.img}" class="img__book" alt="${item.name}"> ${item.name}
+                        <button class="removeFromPlans" data-index="${index}">Удалить</button>
+                    </div>
+                </div>
+            </div>
             </li>`
         );
     });
@@ -15,12 +21,12 @@ $(document).ready(function () {
     }
 
     $('#Plans').on('click', '.removeFromPlans', function () {
-        const index = $(this).data('index'); 
-        Plans.splice(index, 1); 
+        const index = $(this).data('index');
+        Plans.splice(index, 1);
 
         localStorage.setItem('Plans', JSON.stringify(Plans));
 
-        $(this).closest('li').remove(); 
+        $(this).closest('li').remove();
 
         if (Plans.length === 0) {
             $('#Plans').empty().append('<li>Книги не добавлены</li>');
--- public/search.js
+++ public/search.js
@@ -1,32 +1,87 @@
 document.querySelector('#book__card__info').oninput = function () {
-    let val = this.value.trim();
-    let bookCardInfoItems = document.querySelectorAll('.book__card__info p');
+    let val = this.value.trim().toLowerCase(); // Сразу приводим к нижнему регистру
+    // let bookCardItems = document.querySelectorAll('.book__card__info'); // Все карточки
+    let cards = document.querySelectorAll('.book__card')
 
-    if (val !== '') {
-        bookCardInfoItems.forEach(function (elem) {
-            let text = elem.innerText.toLowerCase();
-            let searchVal = val.toLowerCase();
+    for (let i = 0; i < cards.length; i++) {
+        let isMatch = false
+        let name = cards[i].querySelector('.book__name').innerText
+        let author = cards[i].querySelector('.book__loginor').innerText
+        let about = cards[i].querySelector('.book__about').innerText
 
-            if (text.search(searchVal) === -1) {
-                elem.classList.add('hide');
-                elem.innerHTML = elem.innerText;
-            } else {
-                elem.classList.remove('hide');
-                let str = elem.innerText;
-                elem.innerHTML = insertMark(str, text.search(searchVal), val.length);
-            }
-        });
-    } else {
-        bookCardInfoItems.forEach(function (elem) {
-            elem.classList.remove('hide');
-            elem.innerHTML = elem.innerText;
-        });
+
+        if (name.toLowerCase().includes(val)) {
+            isMatch = true
+            insertMark(name, name.toLowerCase().indexOf(val), val.length)
+        }
+        else if (author.toLowerCase().includes(val)) {
+            isMatch = true
+            insertMark(author, author.toLowerCase().indexOf(val), val.length)
+        }
+        else if (about.toLowerCase().includes(val)) {
+            isMatch = true
+            insertMark(about, about.toLowerCase().indexOf(val), val.length)
+        }
+
+        if (!isMatch) {
+            cards[i].classList.add('hide')
+        } else {
+            cards[i].classList.remove('hide')
+        }
+
     }
-}
+
+    // bookCardItems.forEach(function (card) {
+
+    //     let blocks = card.querySelectorAll('p'); // Все текстовые блоки в карточке
+    //     let hasMatch = false; // Флаг для проверки наличия совпадений
+
+    //     blocks.forEach(function (block) {
+    //         let text = block.innerText.toLowerCase(); // Текст блока в нижнем регистре
+
+    //         if (val !== '' && text.includes(val)) {
+    //             hasMatch = true; // Нашли совпадение
+    //             block.classList.remove('hide');
+
+    //             let startPos = text.indexOf(val); // Индекс найденного текста
+    //             block.innerHTML = insertMark(block.innerText, startPos, val.length);
+    //         } else {
+    //             // block.classList.add('hide');
+    //             block.innerHTML = block.innerText; // Восстанавливаем исходный текст
+    //         }
+    //     });
+
+    //     // Если совпадений нет ни в одном из блоков, скрываем всю карточку
+    //     if (hasMatch) {
+    //         card.classList.remove('hide');
+    //     } else {
+    //         card.classList.add('hide');
+    //     }
+    // });
+
+    // Если поле ввода пустое, показываем все карточки и блоки
+    // if (val === '') {
+    //     bookCardItems.forEach(function (card) {
+    //         card.classList.remove('hide');
+    //         let blocks = card.querySelectorAll('p');
+    //         blocks.forEach(function (block) {
+    //             block.classList.remove('hide');
+    //             block.innerHTML = block.innerText; // Восстанавливаем исходный текст
+    //         });
+    //     });
+    // }
+};
 
 function insertMark(string, pos, len) {
-    return string.slice(0, pos) + '<mark>' + string.slice(pos, pos + len) + '</mark>' + string.slice(pos + len);
-};
+    return (
+        string.slice(0, pos) +
+        '<mark>' +
+        string.slice(pos, pos + len) +
+        '</mark>' +
+        string.slice(pos + len)
+    );
+}
+
 
 $(document).ready(function () {
     $('.addToPlans').click(function () {
